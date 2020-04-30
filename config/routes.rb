66a5aef772cc462830d4d7627815d30c59894802
Christian Varriale <varriale737@gmail.com>
2020-04-30 18:35:11 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post '/tools/update_progress', to: 'tools#update_progress'
+  get '/tools/update_progress/:id/:progress', to: 'tools#update_progress'
   get '/tools/get_progress/:id', to: 'tools#get_progress'
   get '/tools/get_video/:id', to: 'tools#get_video'
   post '/tools/download', to: 'tools#download'
