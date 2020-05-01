2354142d421a8b3d580e6eb527441602e2f52453
Christian Varriale <varriale737@gmail.com>
2020-04-30 20:47:31 -0400

    make get request

--- python/youtube_downloader.py
+++ python/youtube_downloader.py
@@ -7,7 +7,7 @@ import os
 def complete_cb(self, fh):
     ''' calls the endpoint with final val '''
 
-    requests.get(f'https://tekblg.com/tools/update_progress/${vid_id}/1', verify=False)
+    requests.get(f'http://tekblg.com/tools/update_progress/${vid_id}/1', verify=False)
 
 
 def progress_cb(self, chunk, fh, bytes_remaining):
@@ -20,7 +20,7 @@ def progress_cb(self, chunk, fh, bytes_remaining):
 
     if time_elasped > 1: # make updated progress request to webhook
         percentage_downloaded = round( ( ( float(fle_size - bytes_remaining) ) / float(fle_size)), 2)
-        requests.get(f'https://tekblg.com/tools/update_progress/{vid_id}/{percentage_downloaded}', verify=False)
+        requests.get(f'http://tekblg.com/tools/update_progress/{vid_id}/{percentage_downloaded}', verify=False)
         start = time.time()
 
 start = time.time()
