41dae126a10029bd89fd0be422a20b11be0fb85b
Christian Varriale <varriale737@gmail.com>
2020-04-30 21:15:07 -0400

    try again

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,2 @@
 class ApiController < ActionController::API
-    skip_before_action :verify_authenticity_token
-    protect_from_forgery prepend: true, with: :exception
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
-    protect_from_forgery prepend: true, with: :exception
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
   
     def fallback_index_html
       render :file => 'public/index.html'
